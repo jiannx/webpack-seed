03e5d5b18a9afa65c05de0903575ee7e819f1dcb
陆建浩 <hzlujianhao@corp.netease.com>
2017-01-20 18:16:37 +0800

    轮播调整

--- src_vue/modules/home/home.js
+++ src_vue/modules/home/home.js
@@ -20,6 +20,7 @@ function init() {
     }).success(() => {
 
     });
+    // banner
     request('indexBanner').success((res) => {
         // homeData.banners = res.rsm.banner_list; // [{titlepic, weburl}]
         homeData.banners = [
@@ -27,23 +28,13 @@ function init() {
             { titlepic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1484836659815&di=de772551083e0c52032cca2c3ab372d0&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201701%2F14%2F8af417550a0b47d94f6977072f35df27.jpg' },
             { titlepic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1484836659815&di=fbeb88dc45d20cbae662f32cfd74dbf6&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201701%2F14%2F552548be1141c5fb907972d46e87ada6.jpg' }
         ];
-        $('#theTarget').append(`<div class="img" style="background-image: url(${homeData.banners[0].titlepic})"></div>`);
-        $('#theTarget').append(`<div class="img" style="background-image: url(${homeData.banners[1].titlepic})"></div>`);
-        $('#theTarget').append(`<div class="img" style="background-image: url(${homeData.banners[2].titlepic})"></div>`);
-        setTimeout(function() {
-            $('#theTarget').skippr({
-                transition: 'fade',
-                speed: 1000,
-                easing: 'easeOutQuart',
-                navType: 'bubble',
-                childrenElementType: 'div',
-                arrows: false,
-                autoPlay: true,
-                autoPlayDuration: 4000,
-                keyboardOnAlways: true,
-                hidePrevious: false
-            }, 200);
+        homeData.banners.forEach((item) => {
+            // $('#theTarget').append(`<div class="img" style="background-image: url(${item.titlepic})"></div>`);
+            $('#theTarget').append(`<div class="img" style="background-image: url(${item.titlepic})"></div>`);
         });
+        setTimeout(function() {
+            $('#theTarget').nivoSlider();
+        }, 200);
     });
 }
 
