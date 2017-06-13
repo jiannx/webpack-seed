2b719ee46d57cec8780738fc99383fcdaef56489
陆建浩 <hzlujianhao@corp.netease.com>
2017-06-13 18:17:02 +0800

    分享页面完善

--- src_angular/share.js
+++ src_angular/share.js
@@ -1,5 +1,11 @@
 import './scss/share.scss';
 
+// http://10.240.35.78:9021/share.html?type=course&id=45&url=weixin://http://weixin.qq.com
+// http://api.mamatuo.com/build/share.html?type=course&id=45&url=mmt://api.mamatuo.com/app/api1.0/found/view
+// http://api.mamatuo.com/build/share.html?type=inviteCode&code=100
+// mmt://api.mamatuo.com/app/api1.0/found/view?courseId=XXX
+// http://127.0.0.1:9021/share.html?type=login&code=100
+// http://127.0.0.1:9021/share.html?type=loginFail
 
 function getQueryString(name) {
   let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
@@ -11,15 +17,47 @@ function getQueryString(name) {
 let type = getQueryString('type');
 let id = getQueryString('id');
 let code = getQueryString('code');
-let data = { type, id, code, rsm: { name: 2 }, isSuccess: true, errMsg: '' };
+let url = getQueryString('url');
+
+let data = {
+  type,
+  id,
+  code,
+  rsm: { name: 2 },
+  isSuccess: true,
+  errMsg: '',
+  login: {
+    checkCodeStatus: 0, // 获取验证码按钮状态
+    isAgree: true, // 是否同意协议
+    data: {
+      telphone: '',
+      validCode: '',
+      password: '',
+      invite_code: code,
+      agree_deal: 1
+    }
+  }
+};
 
 if (type === 'course') {
-  document.title = '妈妈托 - 课程分享';
+  document.title = '妈妈托-课程分享';
   data.type = 'course';
 }
 if (type === 'inviteCode') {
-  document.title = '邀请码';
+  document.title = '妈妈托-邀请码';
   data.type = 'inviteCode';
+}
+if (type === 'login') {
+  document.title = '妈妈托-注册';
+  data.type = 'login';
+}
+if (type === 'loginSuccess') {
+  document.title = '妈妈托-注册成功';
+  data.type = 'loginSuccess';
+}
+if (type === 'loginFail') {
+  document.title = '妈妈托-注册失败';
+  data.type = 'loginFail';
 }
 
 let vm = new Vue({
@@ -32,9 +70,9 @@ let vm = new Vue({
     this.rsm.name = 3;
     if (type === 'course') {
       $.get('/admin/api/course/share_view/', { id }, function(res) {
-        if (res.errno == -1) {
+        if (res.errno === -1) {
           self.isSuccess = false;
-          self.errMsg = res.err
+          self.errMsg = res.err;
         } else {
           self.$set(data, 'rsm', res.rsm.info);
         }
@@ -42,20 +80,121 @@ let vm = new Vue({
     }
   },
   methods: {
+    // 打开app
     open: function() {
-      console.log('open');
+      let _this = this;
+      if (!url) {
+        alert('url 为空');
+        return;
+      }
       let ifr = document.createElement('iframe');
-      ifr.src = 'weixin://http://weixin.qq.com';
+      ifr.src = url + '&courseId=' + id;
       ifr.style.display = 'none';
       document.body.appendChild(ifr);
       window.setTimeout(function() {
-        console.log('222');
-        var u = navigator.userAgent;
-        var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+        alert('打开失败：' + ifr.src);
         document.body.removeChild(ifr);
-        // window.location = 'http://www.baidu.com';
+        _this.download();
       }, 1000);
+    },
+    // 下载
+    download: function() {
+      let u = navigator.userAgent;
+      let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
+      let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+      if (isAndroid) {
+        // window.location = 'http://www.baidu.com';
+      }
+      if (isiOS) {
+        // window.location = 'http://www.baidu.com';
+      }
+    },
+    // 跳转到登陆页面
+    linkToLogin: function() {
+      window.location = 'share.html?type=login&code=' + code;
+    },
+    linkToLoginSuccess: function() {
+      window.location = 'share.html?type=loginSuccess';
+    },
+    linkToLoginFail: function() {
+      window.location = 'share.html?type=loginFail';
+    },
+    // 获取验证码读秒
+    validTiming: function() {
+      let self = this;
+      setTimeout(function() {
+        data.login.checkCodeStatus = data.login.checkCodeStatus - 1;
+        if (data.login.checkCodeStatus > 0) {
+          self.validTiming();
+        }
+      }, 1000);
+    },
+    // 获取验证码
+    getValidCode: function() {
+      let _this = this;
+      $.ajax({
+        url: '/app/api1.0/login/get_validcode/',
+        type: 'post',
+        data: {
+          telphone: data.login.data.telphone,
+          types: 1,
+          // app_types: 2
+        },
+        dataType: 'json',
+        success: function(res) {
+          if (res.errno === -1) {
+            alert(res.err);
+          } else {
+            data.login.checkCodeStatus = 60;
+            _this.validTiming();
+          }
+        }
+      });
+    },
+    // 注册
+    signUp: function() {
+      let _this = this;
+      let d = data.login.data;
+      if (d.telphone === '') {
+        alert('请输入手机号');
+        return;
+      }
+      if (d.validCode === '') {
+        alert('请输入验证码');
+        return;
+      }
+      if (d.password === '') {
+        alert('请设置密码');
+        return;
+      }
+      $.ajax({
+        url: '/app/api1.0/login/get_user_register/',
+        type: 'post',
+        data: d,
+        dataType: 'json',
+        success: function(res) {
+          if (res.errno === -1) {
+            alert(res.err);
+          } else {
+            _this.linkToLoginSuccess();
+          }
+        }
+      });
+    },
+    toggleAgree: function() {
+      if (this.login.data.agree_deal === 0) {
+        this.login.data.agree_deal = 1;
+      } else {
+        this.login.data.agree_deal = 0;
+      }
+    }
+  },
+  computed: {
+    validMsg: function() {
+      if (data.login.checkCodeStatus === 0) {
+        return '获取验证码';
+      }
+      return data.login.checkCodeStatus + 's后重新获取';
     }
   }
 });
